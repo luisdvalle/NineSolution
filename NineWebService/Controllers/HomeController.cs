6bfbdb10089a5b344675be21ad24beaaeab5ae7a
luisdvalle <jums.delvalle@gmail.com>
2018-03-13 16:51:52 +1100

    Service dependency injection
    in HomeController.

--- NineWebService/Controllers/HomeController.cs
+++ NineWebService/Controllers/HomeController.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using NineWebService.Abstractions;
 using NineWebService.Models;
 
 namespace NineWebService.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Data")]
     public class HomeController : Controller
     {
+        private IDataService _dataService;
+
+        public HomeController(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
         [HttpPost]
         public IActionResult Index([FromBody]Root root)
         {
--- NineWebService/Services/DataService.cs
+++ NineWebService/Services/DataService.cs
@@ -1,9 +1,6 @@
 ﻿using NineWebService.Abstractions;
 using NineWebService.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NineWebService.Services
 {

7f03670fc4d25bcf30cc5937996afa561e8c0853
luisdvalle <jums.delvalle@gmail.com>
2018-03-13 16:26:38 +1100

    Initial commit of DataService class
    Initial implementation of DataService class with the concrete
    implementation to process a Root object

---
+++ NineWebService/Services/DataService.cs
@@ -0,0 +1,17 @@
+﻿using NineWebService.Abstractions;
+using NineWebService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NineWebService.Services
+{
+    public class DataService : IDataService
+    {
+        public Response ProcessIncomingShowJsonString(Root root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

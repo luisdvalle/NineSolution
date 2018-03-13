9ee447c8e281fa8a41180e0a4a06c30d0faf4a4d
luisdvalle <jums.delvalle@gmail.com>
2018-03-13 16:39:32 +1100

    Initial commit HomeController
    Initial implementation of the Home controller

---
+++ NineWebService/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NineWebService.Models;
+
+namespace NineWebService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Data")]
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index([FromBody]Root root)
+        {
+            return Ok();
+        }
+    }
+}

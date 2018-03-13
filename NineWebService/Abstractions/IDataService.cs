71c6064d19aa347e74d8e606df5a4277886a8e3b
luisdvalle <jums.delvalle@gmail.com>
2018-03-13 16:25:11 +1100

    Interface for DataService
    Describes the methods necessary to process an incoming JSON string with
    information about shows

---
+++ NineWebService/Abstractions/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using NineWebService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NineWebService.Abstractions
+{
+    public interface IDataService
+    {
+        Response ProcessIncomingShowJsonString(Root root);
+    }
+}

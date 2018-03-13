4a4d3c9f649c771f49a1d1ce09889c88f00fe45a
luisdvalle <jums.delvalle@gmail.com>
2018-03-13 16:27:34 +1100

    Model classes for Responses

---
+++ NineWebService/Models/Response.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NineWebService.Models
+{
+    public class Response
+    {
+        public Show[] Shows { get; set; }
+    }
+}
---
+++ NineWebService/Models/Show.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NineWebService.Models
+{
+    public class Show
+    {
+        public Image Image { get; set; }
+        public string Slug { get; set; }
+        public string Title { get; set; }
+    }
+}

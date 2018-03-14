a02114cac860390a3d18eaa5ef206250ad1f0105
luisdvalle <jums.delvalle@gmail.com>
2018-03-14 20:43:34 +1100

    Renaming DataService.cs
    Renaming DataService.cs to DataProcessor.cs

---
+++ NineWebService/Services/DataProcessor.cs
@@ -0,0 +1,14 @@
+﻿using DataService.Abstractions;
+using NineWebService.Models;
+using System;
+
+namespace NineWebService.Services
+{
+    public class DataProcessor : IDataProcessor<>
+    {
+        public Response ProcessIncomingShowData(IncomingData requestData)
+        {
+            return null;
+        }
+    }
+}

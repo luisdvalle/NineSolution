a892309715b196f9dba758583ff56e2ebe892497
luisdvalle <jums.delvalle@gmail.com>
2018-03-15 11:44:26 +1100

    Implementation on DataProcessor and refactoring name

---
+++ NineWebService/Services/RequestDataProcessor.cs
@@ -0,0 +1,29 @@
+﻿using DataService.Abstractions;
+using NineWebService.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NineWebService.Services
+{
+    public class RequestDataProcessor : IDataProcessor<Payload>
+    {
+        public IEnumerable<Response> ProcessRequestData(IEnumerable<Payload> requestData)
+        {
+            var requestDataArray = requestData.ToArray();
+
+            Payload[] payload = new Payload[requestDataArray.Length];
+
+            for (int i = 0; i < requestDataArray.Length ; i++)
+            {
+                payload[i] = requestDataArray[i];
+            }
+
+            Show[] shows = payload
+                .Where(p => p.Drm == true && p.EpisodeCount > 0)
+                .Select(p => new Show { Image = p.Image.ShowImage, Slug = p.Slug, Title = p.Title })
+                .ToArray();
+
+            return shows;
+        }
+    }
+}

19590873d6bd745f2656902b077c84494e80684b
luisdvalle <jums.delvalle@gmail.com>
2018-03-15 11:42:26 +1100

    Removing unused using references
    In Model Classes

--- NineWebService/Models/Season.cs
+++ NineWebService/Models/Season.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace NineWebService.Models
+﻿namespace NineWebService.Models
 {
     public class Season
     {
